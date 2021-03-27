a1411d8ed41dcedc4ecbc0d109cc205e3605d8ec
Robin Hu <robinfhu@gmail.com>
2021-03-27 15:17:11 -0500

    Adding a header

--- src/header.js
+++ src/header.js
@@ -1,18 +1,35 @@
 
 export default class Header extends React.Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+            navItems: [
+                {name: 'Home'}
+            ,
+                {name: 'About'}
+            ],
+            active: 'Home'
+        }
+    }
     render() {
+        const navItems = this.state.navItems.map((d)=> {
+            const active = (this.state.active === d.name) ? 'active' : ''
+
+            return <li className='nav-item me-3'>
+                <a className={`nav-link ${active}`}>{d.name}</a>
+            </li>
+        })
         return <header>
             <div className='navbar navbar-dark bg-dark shadow-sm'>
-                <div className='container justify-content-start'>
+                <div className='container-fluid justify-content-start'>
                     <a href="#" className='navbar-brand d-flex align-items-center'>
                         {this.props.brand || "React Boilerplate"}
                     </a>
 
-                    <div className=''>
-                        <ul className='navbar-nav'>
-                            <li className='nav-item'>
-                                <a className='nav-link'>Page 1</a>
-                            </li>
+                    <div>
+                        <ul className='navbar-nav flex-row'>
+                            {navItems}
                         </ul>
                     </div>
                 </div>
