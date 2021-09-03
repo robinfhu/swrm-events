e04ede8f1175e860c18f7d4709269d2834388434
Robin Hu <robinfhu@gmail.com>
2021-09-02 19:01:03 -0500

    Install bootstrap and add collapsible navbar

--- src/header/index.js
+++ src/header/index.js
@@ -25,17 +25,20 @@ export default class Header extends React.Component {
             </li>
         })
         return <header>
-            <div className='navbar navbar-dark bg-dark shadow-sm'>
+            <div className='navbar navbar-expand-lg navbar-dark bg-dark shadow-sm'>
                 <div className='container-fluid justify-content-start'>
-                    <a href="#" className='navbar-brand d-flex align-items-center'>
-                        {this.props.brand || "React Boilerplate"}
-                    </a>
-
-                    <div>
-                        <ul className='navbar-nav flex-row'>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarAppHeader" aria-controls="navbarAppHeader" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarAppHeader">
+                        <a href="#" className='navbar-brand d-flex align-items-center'>
+                            {this.props.brand || "React Boilerplate"}
+                        </a>
+                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
                             {navItems}
                         </ul>
                     </div>
+                    
                 </div>
             </div>
         </header>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import Header from "./header"
 import {UserGrid, generateData} from "./user-mgmt-example.js"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import HelloWorld from "./hello-world.js"
 
 const users = generateData()
 
 const layout = <React.Fragment>
-    <Header brand='My App' />
-    <UserGrid users={users} />
+    <Header brand='SWRM 2021' />
+    <HelloWorld users={users} name="Robin" />
     </React.Fragment>
 
 
